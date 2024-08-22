571acd15c935e768868851d805809626e5b1535f
rodrigo baptista <rodrigo.baptista@zup.com.br>
2024-08-22 15:41:37 -0300

    V1 Prd rollback action

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
-# runtime-rollback-action
+# runtime-manager-action
+
+GitHub action to authenticate and consume StackSpot Runtime API.
+
+_**Note**: This action is supported on all runners operating systems (`ubuntu`, `macos`, `windows`)_
+
+## 📚 Usage
+
+### Requirements
+
+To get the account keys (`CLIENT_ID`, `CLIENT_KEY` and `CLIENT_REALM`), please login using a **ADMIN** user on the [StackSpot Portal](https://stackspot.com), and generate new keys at [https://stackspot.com/en/settings/access-token](https://stackspot.com/en/settings/access-token).
+
+### Use Case
+
+```yaml
+    steps:
+      - uses: stack-spot/runtime-manager-action@v2.1
+        id: run
+        with:
+          CLIENT_ID: ${{ secrets.CLIENT_ID }}
+          CLIENT_KEY: ${{ secrets.CLIENT_KEY }}
+          CLIENT_REALM: ${{ secrets.CLIENT_REALM }}
+          WORKSPACE: my_workspace
+          ENVIRONMENT: my_environment
+          VERSION_TAG: my_tag
+          TF_STATE_BUCKET_NAME: my_bucket
+          TF_STATE_REGION: region
+          IAC_BUCKET_NAME: my_bucket
+          IAC_REGION: region
+          VERBOSE: true # not mandatory
+          BRANCH: main # not mandatory
+          OPEN_API_PATH: swagger.yaml # not mandatory
+          DYNAMIC_INPUTS: --key1 value1 --key2 value2
+          WORKDIR: ./my-folder # not mandatory
+
+      - name: Check Run Tasks Data
+        run: echo "Tasks = ${{ steps.run.outputs.tasks }}"
+        shell: bash
+```
+
+* * *
+
+## ▶️ Action Inputs
+
+Field | Mandatory | Default Value | Observation
+------------ | ------------  | ------------- | -------------
+**CLIENT_ID** | YES | N/A | [StackSpot](https://stackspot.com/en/settings/access-token) Client ID.
+**CLIENT_KEY** | YES | N/A |[StackSpot](https://stackspot.com/en/settings/access-token) Client KEY.
+**CLIENT_REALM** | YES | N/A |[StackSpot](https://stackspot.com/en/settings/access-token) Client Realm.
+**WORKSPACE** | YES | N/A | StackSpot Workspace where the project has been registered.
+**ENVIRONMENT** | YES | N/A | StackSpot Environment where the project will be deployed.
+**VERSION_TAG** | YES | N/A | Deploy version tag
+**TF_STATE_BUCKET_NAME** | YES | N/A | AWS S3 Bucket name where the generated tfstate files will be stored.
+**TF_STATE_REGION** | YES | N/A | AWS region where the TF State will be stored (e.g: `us-east-1`).
+**IAC_BUCKET_NAME** | YES | N/A | AWS S3 Bucket name where the generated IaC files will be stored.
+**IAC_REGION** | YES | N/A | AWS region where the IaC will be stored (e.g: `us-east-1`).
+**VERBOSE** | NO | `false` | Whether to show extra logs during execution. (e.g: `true`).
+**BRANCH** | NO | N/A | Repository branch to checkout if necessary (e.g: `main`).
+**OPEN_API_PATH** | NO | N/A | Path to OpenAI / Swagger file within the repository (e.g: `path/to/file.yml`)
+**DYNAMIC_INPUTS** | NO | N/A | Dynamic inputs used with Jinja on IAC, informed as `--key1 value1 --key2 value2`
+**WORKDIR** | NO | ./ | Path to the directory where the `.stk` is located.
+
+* * *
+
+## License
+
+[Apache License 2.0](https://github.com/stack-spot/runtime-manager-action/blob/main/LICENSE)
---
+++ runtime.py
@@ -0,0 +1,156 @@
+import os
+import requests
+import json
+from pathlib import Path
+from ruamel.yaml import YAML
+from io import StringIO
+
+
+def yaml() -> YAML:
+    yml = YAML()
+    yml.indent(mapping=2, sequence=4, offset=2)
+    yml.allow_unicode = True
+    yml.default_flow_style = False
+    yml.preserve_quotes = True
+    return yml
+
+
+def safe_load(content: str) -> dict:
+    yml = yaml()
+    return yml.load(StringIO(content))
+
+
+def save_output(name: str, value: str):
+    with open(os.environ['GITHUB_OUTPUT'], 'a') as output_file:
+        print(f'{name}={value}', file=output_file)
+
+
+def build_pipeline_url() -> str:
+    GITHUB_SERVER_URL = os.getenv("GITHUB_SERVER_URL")
+    GITHUB_REPOSITORY = os.getenv("GITHUB_REPOSITORY")
+    GITHUB_RUN_ID = os.getenv("GITHUB_RUN_ID")
+    if None in [GITHUB_SERVER_URL, GITHUB_REPOSITORY, GITHUB_RUN_ID]:
+        print("- Some mandatory GitHub Action environment variable is empty.")
+        exit(1)
+    url = f"{GITHUB_SERVER_URL}/{GITHUB_REPOSITORY}/actions/runs/{GITHUB_RUN_ID}"
+    return url
+
+
+ACTION_PATH = os.getenv("ACTION_PATH")
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_KEY = os.getenv("CLIENT_KEY")
+CLIENT_REALM = os.getenv("CLIENT_REALM")
+TF_STATE_BUCKET_NAME = os.getenv("TF_STATE_BUCKET_NAME")
+TF_STATE_REGION = os.getenv("TF_STATE_REGION")
+IAC_BUCKET_NAME = os.getenv("IAC_BUCKET_NAME")
+IAC_REGION = os.getenv("IAC_REGION")
+VERBOSE = os.getenv("VERBOSE")
+VERSION_TAG = os.getenv("VERSION_TAG")
+ENVIRONMENT = os.getenv("ENVIRONMENT")
+
+inputs_list = [ENVIRONMENT, VERSION_TAG, ACTION_PATH, CLIENT_ID, CLIENT_KEY, CLIENT_REALM, TF_STATE_BUCKET_NAME, TF_STATE_REGION, IAC_BUCKET_NAME, IAC_REGION]
+
+if None in inputs_list:
+    print("- Some mandatory input is empty. Please, check the input list.")
+    exit(1)
+
+with open(Path(ACTION_PATH+'/.stk/stk.yaml'), 'r') as file:
+    stk_yaml = file.read()
+
+stk_dict = safe_load(stk_yaml)
+
+if VERBOSE is not None:
+    print("- stk.yaml:", stk_dict)
+
+stk_yaml_type = stk_dict["kind"]
+app_or_infra_id = stk_dict["spec"]["infra-id"] if stk_yaml_type == "infra" else stk_dict["spec"]["app-id"]
+
+print(f"{stk_yaml_type} project identified, with ID: {app_or_infra_id}")
+
+iam_url = f"https://auth.stackspot.com/{CLIENT_REALM}/oidc/oauth/token"
+iam_headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+iam_data = {"client_id": f"{CLIENT_ID}", "grant_type": "client_credentials", "client_secret": f"{CLIENT_KEY}"}
+
+print("Authenticating...")
+r1 = requests.post(
+        url=iam_url, 
+        headers=iam_headers, 
+        data=iam_data
+    )
+
+if r1.status_code == 200:
+    d1 = r1.json()
+    access_token = d1["access_token"]
+    
+    print("Successfully authenticated!")
+
+    request_data = {
+        "appId": app_or_infra_id if stk_yaml_type != "infra" else None,
+        "infraId": app_or_infra_id if stk_yaml_type == "infra" else None,
+        "env": ENVIRONMENT,
+        "tag": VERSION_TAG,
+        "config": {
+            "tfstate": {
+                "bucket": TF_STATE_BUCKET_NAME,
+                "region": TF_STATE_REGION
+            },
+            "iac": {
+                "bucket": IAC_BUCKET_NAME,
+                "region": IAC_REGION
+            }
+        },
+        "pipelineUrl": build_pipeline_url()
+    }
+
+    request_data = json.dumps(request_data)
+
+    if VERBOSE is not None:
+        print("- ROLLBACK RUN REQUEST DATA:", request_data)
+    
+    deploy_headers = {"Authorization": f"Bearer {access_token}", "Content-Type": "application/json"}
+
+    print("Deploying Self-Hosted Rollback..")
+
+    if stk_yaml_type == 'app':
+        self_hosted_rollback_app_url = "https://runtime-manager.v1.stackspot.com/v1/run/self-hosted/rollback/app"
+        rollback_request = requests.post(
+                url=self_hosted_rollback_app_url,
+                headers=deploy_headers,
+                data=request_data
+            )
+    elif stk_yaml_type == 'infra':
+        self_hosted_rollback_infra_url = "https://runtime-manager.v1.stackspot.com/v1/run/self-hosted/rollback/infra"
+        rollback_request = requests.post(
+                url=self_hosted_rollback_infra_url,
+                headers=deploy_headers,
+                data=request_data
+            )
+    else:
+        print("- STK TYPE not recognized. Please, check the input.")
+        exit(1)
+
+    if rollback_request.status_code == 201:
+        d2 = rollback_request.json()
+        runId = d2["runId"]
+        runType = d2["runType"]
+        tasks = d2["tasks"]
+
+        save_output('tasks', tasks)
+        save_output('run_id', runId)
+
+        print(f"- Rollback RUN {runType} successfully started with ID: {runId}")
+        print(f"- Rollback RUN TASKS LIST: {tasks}")
+
+    else:
+        print("- Error starting self hosted rollback run")
+        print("- Status:", rollback_request.status_code)
+        print("- Error:", rollback_request.reason)
+        print("- Response:", rollback_request.text)
+        exit(1)
+
+else:
+    print("- Error during IAM authentication")
+    print("- Status:", r1.status_code)
+    print("- Error:", r1.reason)
+    print("- Response:", r1.text)
+    exit(1)
